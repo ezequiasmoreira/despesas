7e9457488f3e9e614df98ef8a1f319b9118dd69e
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2021-01-28 00:20:56 -0300

    Finalizado crud da entrada

--- src/main/java/com/ezequias/despesas/resource/EntradaResource.java
+++ src/main/java/com/ezequias/despesas/resource/EntradaResource.java
@@ -1,9 +1,12 @@
 package com.ezequias.despesas.resource;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +28,31 @@ public class EntradaResource {
 	public ResponseEntity<Entrada> salvar(@RequestBody EntradaDTO dto) {
 		Entrada entrada = service.converterParaDTO(dto);
 		entrada = service.salvar(entrada);
+		return ResponseEntity.ok().body(entrada);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody EntradaDTO dto) {		
+		service.atualizar(dto);		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		Entrada entrada = service.obterPorId(id);
+		service.excluir(entrada);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Entrada>> findAll() {
+		List<Entrada> entrada = service.obterTodos();  
+		return ResponseEntity.ok().body(entrada);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Entrada> obterPorId(@PathVariable Integer id) {
+		Entrada entrada = service.obterPorId(id);
 		return ResponseEntity.ok().body(entrada);
 	}
 }
--- src/main/java/com/ezequias/despesas/service/EntradaService.java
+++ src/main/java/com/ezequias/despesas/service/EntradaService.java
@@ -1,5 +1,9 @@
 package com.ezequias.despesas.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,20 +13,45 @@ import com.ezequias.despesas.repository.EntradaRepository;
 
 @Service
 public class EntradaService {
-	
+
 	@Autowired
 	private EntradaRepository entradaRepository;
-	
-	public Entrada converterParaDTO(EntradaDTO dto) {
-		Entrada  entrada = new Entrada();		
+
+	public Entrada converterParaDTO(EntradaDTO dto) {		
+		Entrada entrada = new Entrada();
 		entrada.setDescricao(dto.getDescricao());
 		entrada.setValor(dto.getValor());
-		return entrada;		
+		return entrada;
 	}
-	
-	public Entrada salvar(Entrada entrada) {
+
+	public Entrada salvar(Entrada entrada) {		
 		entrada.setId(null);
 		return entradaRepository.save(entrada);
+	}
+
+	public Entrada atualizar(EntradaDTO entradaDTO) {		
+		Entrada entrada = obterPorId(entradaDTO.getId());
+		entrada.setDescricao(entradaDTO.getDescricao() == null ? entrada.getDescricao() : entradaDTO.getDescricao());
+		entrada.setValor(entradaDTO.getValor() == null ? entrada.getValor() : entradaDTO.getValor());
+		atualizar(entrada);
+		return entrada;
+	}
+
+	public Entrada atualizar(Entrada entrada) {		
+		return entradaRepository.save(entrada);		
+	}
+
+	public void excluir(Entrada entrada){		
+		entradaRepository.deleteById(entrada.getId());
+	}
+
+	public Entrada obterPorId(Integer entradaId) throws ObjectNotFoundException {		
+		Optional<Entrada> entrada = entradaRepository.findById(entradaId);
+		return entrada.orElseThrow(() -> new ObjectNotFoundException("Entrada não encontrado.", null));
+	}
+	
+	public List<Entrada> obterTodos() throws ObjectNotFoundException {		
+		return entradaRepository.findAll();
 	}
 
 }
