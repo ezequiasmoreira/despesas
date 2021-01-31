01666f3d17c43f34eb0459a8ea7030ab9846424f
Ezequias <ezequias.prado@webmaissistemas.com.br>
2021-01-30 22:56:30 -0300

    merge

---
+++ src/main/java/com/ezequias/despesas/service/DBService.java
@@ -0,0 +1,11 @@
+package com.ezequias.despesas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+	public void instanciateTestDataBase() {
+		
+	}
+
+}
