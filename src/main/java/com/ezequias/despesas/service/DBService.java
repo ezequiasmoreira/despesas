7b9ed518535608104b0e67b3a0fda84ae743d29f
Ezequias <ezequiasmoreira@hotmail.com>
2021-01-31 00:27:16 -0300

    Implementado para instanciar as entradas
    Implementado para instanciar as entradas e adicionado o drive para usar
    o banco H2.

--- src/main/java/com/ezequias/despesas/service/DBService.java
+++ src/main/java/com/ezequias/despesas/service/DBService.java
@@ -1,10 +1,30 @@
 package com.ezequias.despesas.service;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ezequias.despesas.domain.Entrada;
+import com.ezequias.despesas.repository.EntradaRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private EntradaRepository entradaRepository;
+	
 	public void instanciateTestDataBase() {
+		
+		Entrada entrada = new Entrada();
+		entrada.setDescricao("Salario");
+		entrada.setValor(3000.00);
+		
+		Entrada entrada2 = new Entrada();
+		entrada2.setDescricao("13° Salario");
+		entrada2.setValor(3000.00);
+		
+		entradaRepository.saveAll(Arrays.asList(entrada,entrada2));		
 		
 	}
 
